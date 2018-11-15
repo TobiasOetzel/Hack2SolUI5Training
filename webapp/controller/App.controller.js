4ef6a7abe7f6b42bcb3ee02ac04cc73b5fd7ad92
Tobias Oetzel <tobias.oetzel@sap.com>
2018-11-15 23:47:14 +0100

    step nr. 2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -15,6 +17,42 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
+		},
+
+
+		/* =========================================================== */
+		/*           begin: event handlers                             */
+		/* =========================================================== */
+
+
+		/**
+		 * Refresh the data.
+		 */
+		onRefresh : function () {
+			var oBinding = this.byId("peopleList").getBinding("items");
+
+			if (oBinding.hasPendingChanges()) {
+				MessageBox.error(this._getText("refreshNotPossibleMessage"));
+				return;
+			}
+			oBinding.refresh();
+			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+
+		/* =========================================================== */
+		/*           end: event handlers                               */
+		/* =========================================================== */
+
+
+		/**
+		 * Convenience method for retrieving a translatable text.
+		 * @param {string} sTextId - the ID of the text to be retrieved.
+		 * @param {Array} [aArgs] - optional array of texts for placeholders.
+		 * @returns {string} the text belonging to the given ID.
+		 */
+		_getText : function (sTextId, aArgs) {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 		}
 	});
 });
