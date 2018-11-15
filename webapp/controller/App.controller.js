2c572c5de3855204d0c533714c768bf016eec81f
Tobias Oetzel <tobias.oetzel@sap.com>
2018-11-15 23:47:16 +0100

    step nr. 4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,8 +2,12 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -13,7 +17,8 @@ sap.ui.define([
 		 */
 		onInit : function () {
 			var oJSONData = {
-				busy : false
+				busy : false,
+				order : 0
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
@@ -37,6 +42,39 @@ sap.ui.define([
 			}
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		/**
+		 * Search for the term in the search field.
+		 */
+		onSearch : function () {
+			var oView = this.getView(),
+				sValue = oView.byId("searchField").getValue(),
+				oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+
+			oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+		},
+
+		/**
+		 * Sort the table according to the last name.
+		 * Cycles between the three sorting states "none", "ascending" and "descending"
+		 */
+		onSort : function () {
+			var oView = this.getView(),
+				aStates = [undefined, "asc", "desc"],
+				aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+				sMessage,
+				iOrder = oView.getModel("appView").getProperty("/order");
+
+			// Cycle between the states
+			iOrder = (iOrder + 1) % aStates.length;
+			var sOrder = aStates[iOrder];
+
+			oView.getModel("appView").setProperty("/order", iOrder);
+			oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			MessageToast.show(sMessage);
 		},
 
 
