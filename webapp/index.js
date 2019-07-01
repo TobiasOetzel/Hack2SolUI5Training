de01e05e3929da0d4ab2e1ded34ff508a4ffd169
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:47:43 -0700

    step nr. 4

--- webapp/index.js
+++ webapp/index.js
@@ -1,20 +1,24 @@
 sap.ui.require([
-	"sap/m/Text",
-	"sap/ui/model/json/JSONModel"
-], function (Text, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView"
+], function (JSONModel, XMLView) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
 		// Create a JSON model from an object literal
 		var oModel = new JSONModel({
-			greetingText: "Hi, my name is Harry Hawk"
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			panelHeaderText: "Data Binding Basics"
 		});
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
-		// Display a text element whose text is derived
-		// from the model object
-		new Text({text: "{/greetingText}"}).placeAt("content");
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.db.view.App"
+		}).placeAt("content");
 	});
 });
