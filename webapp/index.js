f562b6fae330af1d1869126c31f44294bd710008
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:01 -0700

    step nr. 9

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
