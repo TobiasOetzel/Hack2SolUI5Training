d126b4d2959303c607ead76471c803cfc6fb699a
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:47:45 -0700

    step nr. 8

--- webapp/index.js
+++ webapp/index.js
@@ -7,23 +7,26 @@ sap.ui.require([
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
-		// Create a JSON model from an object literal
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
-			enabled: true
+			enabled: true,
+			address: {
+				street: "Dietmar-Hopp-Allee 16",
+				city: "Walldorf",
+				zip: "69190",
+				country: "Germany"
+			}
 		});
 
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
-		// Create a resource bundle for language specific texts
-		var oResourceModel = new ResourceModel({
+		var oResourceBundle = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n"
 		});
 
-		// Assign the model object to the SAPUI5 core using the name "i18n"
-		sap.ui.getCore().setModel(oResourceModel, "i18n");
+		sap.ui.getCore().setModel(oResourceBundle, "i18n");
 
 		// Display the XML view called "App"
 		new XMLView({
