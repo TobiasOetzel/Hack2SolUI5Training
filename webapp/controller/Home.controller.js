82e0358c2258ad16f293cb27dd180ce0d5e9a9e2
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:48:21 -0700

    step nr. 6

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,6 +10,10 @@ sap.ui.define([
 			this.getRouter().getTargets().display("notFound", {
 				fromTarget : "home"
 			});
+		},
+
+		onNavToEmployees : function () {
+			this.getRouter().navTo("employeeList");
 		}
 
 	});
