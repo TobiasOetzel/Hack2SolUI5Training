94c22dc5803bc67172a6351e5db521d4b4653f01
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:15 -0700

    step nr. 33

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
 	"sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+], function (Controller, History, UIComponent) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
@@ -16,6 +17,18 @@ sap.ui.define([
 				path: "/" + oEvent.getParameter("arguments").invoicePath,
 				model: "invoice"
 			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
