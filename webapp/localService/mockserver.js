058483c38600b45c2807f8bd17fdb4ed9b13c958
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:48:03 -0700

    step nr. 4

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,7 +1,8 @@
 sap.ui.define([
+	"sap/ui/thirdparty/jquery",
 	"sap/ui/core/util/MockServer",
 	"sap/base/Log"
-], function(MockServer, Log) {
+], function(jQuery, MockServer, Log) {
 	"use strict";
 
 	return {
@@ -21,6 +22,30 @@ sap.ui.define([
 				sMockdataBaseUrl: "../localService/mockdata",
 				bGenerateMissingMockData: true
 			});
+
+			// handling mocking a function import call step
+			var aRequests = oMockServer.getRequests();
+			aRequests.push({
+				method: "GET",
+				path: new RegExp("FindUpcomingMeetups(.*)"),
+				response: function(oXhr) {
+					Log.debug("Incoming request for FindUpcomingMeetups");
+					var today = new Date();
+					today.setHours(0); // or today.toUTCString(0) due to timezone differences
+					today.setMinutes(0);
+					today.setSeconds(0);
+					jQuery.ajax({
+						url: "/Meetups?$filter=EventDate ge " + "/Date(" + today.getTime() + ")/",
+						dataType : 'json',
+						async: false,
+						success : function(oData) {
+							oXhr.respondJSON(200, {}, JSON.stringify(oData));
+						}
+					});
+					return true;
+				}
+			});
+			oMockServer.setRequests(aRequests);
 
 			// handling custom URL parameter step
 			var fnCustom = function(oEvent) {
