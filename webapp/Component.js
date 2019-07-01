60126671c809012e87478a0a390b0e1080d913b2
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:11 -0700

    step nr. 28

--- webapp/Component.js
+++ webapp/Component.js
@@ -7,18 +7,18 @@ sap.ui.define([
 
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 
-		metadata : {
+		metadata: {
 			manifest: "json"
 		},
 
-		init : function () {
+		init: function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// set data model
 			var oData = {
-				recipient : {
-					name : "World"
+				recipient: {
+					name: "World"
 				}
 			};
 			var oModel = new JSONModel(oData);
