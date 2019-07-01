fb05b75c1c9e6312ebf22408581d5f1fa217d9b3
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:01 -0700

    step nr. 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,23 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+		onInit : function () {
+
+			// set data model on view
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 
 		onShowHello : function () {
 			MessageToast.show("Hello World");
