3cf34a9c4085276f8f4f0b9872d89290132251c3
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:47:48 -0700

    step nr. 15

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define([
 	"sap/m/library",
 	"sap/ui/core/Locale",
 	"sap/ui/core/LocaleData",
-	"sap/ui/model/type/Currency"
-], function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
+	"sap/ui/model/type/Currency",
+	"sap/m/ObjectAttribute"
+], function (Controller, mobileLibrary, Locale, LocaleData, Currency, ObjectAttribute) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.db.controller.App", {
@@ -15,19 +16,45 @@ sap.ui.define([
 				oBundle.getText("mailSubject", [sFirstName]),
 				oBundle.getText("mailBody"));
 		},
-		formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
+
+		formatStockValue : function(fUnitPrice, iStockLevel, sCurrCode) {
 			var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
 			var oLocale = new Locale(sBrowserLocale);
 			var oLocaleData = new LocaleData(oLocale);
 			var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
 			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
 		},
-		onItemSelected: function(oEvent) {
+
+		onItemSelected : function(oEvent) {
 			var oSelectedItem = oEvent.getSource();
 			var oContext = oSelectedItem.getBindingContext("products");
 			var sPath = oContext.getPath();
 			var oProductDetailPanel = this.byId("productDetailsPanel");
 			oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+		},
+
+		productListFactory : function(sId, oContext) {
+			var oUIControl;
+
+			// Decide based on the data which dependant to clone
+			if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")) {
+				// The item is discontinued, so use a StandardListItem
+				oUIControl = this.byId("productSimple").clone(sId);
+			} else {
+				// The item is available, so we will create an ObjectListItem
+				oUIControl = this.byId("productExtended").clone(sId);
+
+				// The item is temporarily out of stock, so we will add a status
+				if (oContext.getProperty("UnitsInStock") < 1) {
+					oUIControl.addAttribute(new ObjectAttribute({
+						text : {
+							path: "i18n>outOfStock"
+						}
+					}));
+				}
+			}
+
+			return oUIControl;
 		}
 	});
 });
