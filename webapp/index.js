4ac6d60e2a855685750479fcb9090b75a5039275
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:47:47 -0700

    step nr. 12

--- webapp/index.js
+++ webapp/index.js
@@ -7,6 +7,10 @@ sap.ui.require([
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
+		var oProductModel = new JSONModel();
+		oProductModel.loadData("./model/Products.json");
+		sap.ui.getCore().setModel(oProductModel, "products");
+
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
