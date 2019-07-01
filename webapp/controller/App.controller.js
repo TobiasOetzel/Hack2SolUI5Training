72b1cce05c55af0b240d7934cdef9a2b4aee3037
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:48:19 -0700

    step nr. 4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/demo/nav/controller/BaseController"
+], function (BaseController) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.nav.controller.App", {
+	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 
 		onInit: function () {
 
