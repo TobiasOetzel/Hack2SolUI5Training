421a2243a9e771085116e497975277880b48f749
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:59 -0700

    step nr. 16

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -14,6 +15,25 @@ sap.ui.define([
 
 			// show message
 			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog : function () {
+			var oView = this.getView();
+
+			// create dialog lazily
+			if (!this.byId("helloDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			}
 		}
 	});
 
