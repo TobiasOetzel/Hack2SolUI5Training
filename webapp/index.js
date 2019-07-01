1e7cb6c3b6092eeb200174e17ad79f0717044734
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:53 -0700

    step nr. 4

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,11 @@
 sap.ui.define([
-	"sap/m/Text"
-], function (Text) {
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
-	new Text({
-		text: "Hello World"
-	}).placeAt("content");
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 });
