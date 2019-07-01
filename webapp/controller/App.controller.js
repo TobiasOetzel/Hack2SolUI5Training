3a568b86feffbba75fc5f96c1eefeca78ac31715
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:45:00 -0700

    step nr. 6

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
 		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
+			MessageToast.show("Hello World");
 		}
 	});
 
