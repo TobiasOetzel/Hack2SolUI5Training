f59c2b977f2b896d6d1c34571153ab1a9c839a9c
Tobias Oetzel <tobias.oetzel@sap.com>
2019-07-01 01:47:43 -0700

    step nr. 3

--- webapp/index.js
+++ webapp/index.js
@@ -10,11 +10,11 @@ sap.ui.require([
 		var oModel = new JSONModel({
 			greetingText: "Hi, my name is Harry Hawk"
 		});
-
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
-		// Create a text UI element that displays a hardcoded text string
-		new Text({text: "Hi, my name is Harry Hawk"}).placeAt("content");
+		// Display a text element whose text is derived
+		// from the model object
+		new Text({text: "{/greetingText}"}).placeAt("content");
 	});
 });
